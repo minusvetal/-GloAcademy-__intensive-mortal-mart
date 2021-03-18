9a0f8f20efd74d5d19280e8ba21262184d233d18
minusvetal <68374186+minusvetal@users.noreply.github.com>
2021-03-18 16:16:17 +0200

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,4 +1,13 @@
-// new WOW().init();
+$ (document).ready(function(){
+   $('#container').vide({
+    mp4: 'video/video.mp4',
+    webm: 'video/video.webm',
+    ogv: 'video/video.ogv',
+    poster: 'img/cover-image.png'
+  });
+})
+
+
 const orderFilm = document.querySelector("#order");
 const modal = document.querySelector(".modal");
 const close = document.querySelector(".close");
@@ -9,4 +18,5 @@ close.addEventListener("click", toggleModal);
 function toggleModal() {
   modal.classList.toggle("is-open");
 }
+
 
